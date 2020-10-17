d479928a4a5a54e80c61f3e9bbd67dc69064d39d
Sina Kamali <Sina Kamali>
2020-10-17 10:54:08 +0330

    post tahvil fix

--- xv6-public-master/lcm.c
+++ xv6-public-master/lcm.c
@@ -6,7 +6,7 @@ int lcm_of_two (int num1, int num2){
 	int max_of_two = (num1 > num2)? num1 : num2;
 	while (1){
 		if(max_of_two % num1 == 0 && max_of_two % num2 == 0){
-    	return max_of_two;
+    		return max_of_two;
 		}
 		++max_of_two;
 	}
@@ -26,10 +26,10 @@ int
 main(int argc, char *argv[])
 {
 
-  if(argc < 3){
-    printf(2, "lcm needs at least 2 argumants\n");
-    exit();
-  }
+	if(argc < 3){
+		printf(2, "lcm needs at least 2 argumants\n");
+		exit();
+	}
 	if (argc > 9){
 		printf(2, "The maximum number of arguments is 8\n");
     exit();
@@ -39,9 +39,10 @@ main(int argc, char *argv[])
 		lcm = lcm_of_two(atoi(argv[i]), lcm);
 	}
 
-	int fd = open("lcm_result.txt",0x001 | 0x200); 
-	write(fd, itoa(lcm,10),sizeof(itoa(lcm,10)));
-	write(fd,"\n",1);
+	int fd = open("lcm_result.txt", 0x001 | 0x200); 
+	printf(2,"%d\n",lcm);
+	write(fd, itoa(lcm,10),sizeof(itoa(lcm,10)) + 3);
+	write(fd,"\n",2);
 	close(fd);
   exit();
 }
