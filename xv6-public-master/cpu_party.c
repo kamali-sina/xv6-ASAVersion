2c81e1bc512744458ea626ac587acca5a91574bd
Alireza <a.r.ebrahimi79@gmail.com>
2020-12-06 16:40:55 +0330

    changed cpu_party to as it shall be

--- xv6-public-master/cpu_party.c
+++ xv6-public-master/cpu_party.c
@@ -10,12 +10,21 @@ int
 main(int argc, char *argv[])
 {
     int booze = 100;
-	while (1){
+    int shots;
+    int drunk = 50;
+	for(shots = 0; shots < drunk; shots++) {
         int pid = fork();
         if (pid == 0){
             for (int i = 0; i < 1000000; i++){
                 booze = drink(booze);
+                exit();
             }
+        }
+        else{
+            for (int i = 0; i < 100000; i++){
+                booze = drink(booze);
+            }
+            wait();
         }
     }
     exit();
